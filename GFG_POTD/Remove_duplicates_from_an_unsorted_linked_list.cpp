6f4a6f13c6cd13072d8cf4eebb553eabe31ba24d
ManjuAnupoju <107555916+ManjuAnupoju@users.noreply.github.com>
2023-07-23 22:43:03 +0530

    Create Remove_duplicates_from_an_unsorted_linked_list.cpp

---
+++ GFG_POTD/Remove_duplicates_from_an_unsorted_linked_list.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+     // your code goes here
+        struct Node* p=head,*q=NULL;
+        map<int,int> m;
+        
+        while(p!=NULL)
+        {
+            if(m.count(p->data))
+            {
+                q->next=p->next;
+                p->next=NULL;
+                p=q->next;
+            }
+            else{
+                m[p->data]=1;
+                q=p;
+                p=p->next;
+            }
+        }
+        return head;
+    }
+};
