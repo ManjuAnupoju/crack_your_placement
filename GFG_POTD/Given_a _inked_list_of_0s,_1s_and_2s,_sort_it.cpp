19e96109854258b099c4fee31f41e0ee1c33e310
ManjuAnupoju <107555916+ManjuAnupoju@users.noreply.github.com>
2023-07-23 20:39:23 +0530

    Day-22/100

---
+++ GFG_POTD/Given_a _inked_list_of_0s,_1s_and_2s,_sort_it.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        
+        // Add code here
+        struct Node* p=head;
+        int arr[1000000],i=0;
+        while(p!=NULL)
+        {
+            arr[i]=p->data;
+            p=p->next;
+            i++;
+        }
+        sort(arr,arr+i);
+        p=head;
+        i=0;
+        while(p!=NULL)
+        {
+            p->data=arr[i];
+            i++;
+            p=p->next;
+        }
+        return head;
+        
+    }
+};
